df7cd8b588a2bc691d418384399583a38023a55e
Arbaz-khan786 <ak0297305@gmail.com>
2026-05-22 18:09:45 +0530

    Fixed production CORS

--- src/config/CORS.js
+++ src/config/CORS.js
@@ -1,7 +1,18 @@
 import cors from "cors";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://shophub-frontend-azure.vercel.app",
+];
+
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
 };
 
